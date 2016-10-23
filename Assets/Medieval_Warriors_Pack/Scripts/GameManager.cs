8e9df7e16982206d9fe5c791c21e9d8643836a3d
張彪 <z.angus0121@gmail.com>
2016-10-23 21:40:34 +0900

    ソースコミット

---
+++ Assets/Medieval_Warriors_Pack/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public Transform EnemyHome;
+	public Transform KnightHome;
+
+
+	// Use this for initialization
+	void Start () {
+		
+		GameObject Enemy = Instantiate(Resources.Load ("Prefabs/Knight2_Sword")) as GameObject;
+		Enemy.transform.SetParent (EnemyHome);
+		Enemy.transform.position = EnemyHome.localPosition;
+
+		// Knight作成
+		GameObject Knight = Instantiate(Resources.Load ("Prefabs/Knight1_Sword")) as GameObject;
+		Knight.transform.SetParent (KnightHome);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+//		Debug.Log ("1");
+//		GameObject Enemy  = (GameObject)Resources.Load("Prefabs/Knight2_Sword");
+//		Instantiate (Enemy, Vector3.zero, Quaternion.identity);
+//		Enemy.transform.SetParent (EnemyHome);
+	}
+}
